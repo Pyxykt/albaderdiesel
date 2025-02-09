6a5f6316147b7ed6d7ee5680c1ed80fb33777fbc
Mohammed Sohail <mohammedisohail313@gmail.com>
2025-02-09 16:17:28 +0530

    video added

--- src/components/Home/overView.tsx
+++ src/components/Home/overView.tsx
@@ -7,23 +7,34 @@ const OverView = () => {
         <div className="space-y-8">
           <h1 className="text-primary-foreground">Overview</h1>
           <p className="text-primary-foreground">
-          Founded in 2014, Al Bader Diesel Trading LLC has been a trusted name in fuel distribution across the UAE. Serving all Emirates, we supply high-quality petroleum products to large industries, Government sector, civil construction projects, and transportation companies, ensuring seamless operations for businesses that drive the nation’s progress.
+            Founded in 2014, Al Bader Diesel Trading LLC has been a trusted name
+            in fuel distribution across the UAE. Serving all Emirates, we supply
+            high-quality petroleum products to large industries, Government
+            sector, civil construction projects, and transportation companies,
+            ensuring seamless operations for businesses that drive the nation’s
+            progress.
           </p>
 
           <p className="text-primary-foreground">
-          With a dedicated fleet of delivery vehicles, we guarantee efficient, timely, and cost-effective fuel supply to meet our clients' growing demands. Our commitment to quality, reliability, and competitive pricing makes us the preferred partner for industries that depend on uninterrupted energy solutions.
+            With a dedicated fleet of delivery vehicles, we guarantee efficient,
+            timely, and cost-effective fuel supply to meet our clients' growing
+            demands. Our commitment to quality, reliability, and competitive
+            pricing makes us the preferred partner for industries that depend on
+            uninterrupted energy solutions.
           </p>
           <p className="text-primary-foreground">
-          At Al Bader Diesel Trading LLC, we don’t just deliver our products, we power businesses, drive efficiency, and success.
+            At Al Bader Diesel Trading LLC, we don’t just deliver our products,
+            we power businesses, drive efficiency, and success.
           </p>
         </div>
 
-        <Image
-          src="/hero.jpeg"
-          alt="Picture of the author"
-          width={500}
-          height={500}
-          className="rounded-lg"
+        <video
+          src="/albadar.mp4" // Ensure the video file is placed in the public folder
+          className="rounded-lg w-[500px] h-[500px] object-cover"
+          autoPlay
+          loop
+          muted
+          playsInline
         />
       </div>
     </section>
